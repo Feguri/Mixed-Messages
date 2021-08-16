09e10f0da55d1009ace2fbe301086844ad158b1a
Felipe Serrou Camy de Paiva <felipeserrou@hotmail.com>
2021-08-16 12:08:01 -0400

    final commit for word complexity.

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ This program will generate a random message.
 // @ts-check
 class randomMessage {
     constructor() {
-        this.nouns = ['apple', 'juice', 'banana', 'fries', 'unicorn', 'potato', 'carrot', 'person', 'building', 'legos', 'Shrek'];
-        this.adjectives = ['lucky', 'delicious', 'bad', 'explosive', 'bananas', 'good', 'flattered', 'angry', 'sad', 'opressed'];
-        this.adjectivesPastTense = ['exploded', 'harmed', 'fished', 'stepped', 'puked', 'nailed', 'jumped', 'fallen'];
-        this.interminglers = ['but', 'that', 'and', 'however', 'of', 'and', 'but', 'that'];
+        this.nouns = ['apple', 'juice', 'banana', 'fries', 'unicorn', 'potato', 'carrot', 'person', 'building', 'legos', 'car', 'book', 'city', 'country',
+                      'Lego set', 'website', 'programming language', 'planet', 'keyboard', 'pony', 'monke', 'food', 'store', 'pool', 'forest', 'bus'];
+        this.adjectives = ['lucky', 'delicious', 'bad', 'explosive', 'bananas', 'good', 'flattered', 'angry', 'sad', 'fun', 'funny', 'amusing', 'incredible',
+                           'excellent', 'vibrant', 'hopeful', 'neat', 'nice', 'notorious', 'fool', 'dumb', 'intelligent', 'charismatic', 'happy'];
+        this.adjectivesPastTense = ['exploded', 'harmed', 'fished', 'stepped', 'puked', 'nailed', 'jumped', 'fallen', 'opressed', 'rotated', 'measured', 
+                                    'dragged', 'destroyed', 'constructed', 'plagiarised', 'contributed', 'worked', 'manufactured', 'broke', 'fixed'];
+        this.interminglers = ['but', 'that', 'and', 'however', 'and', 'but', 'that', 'because'];
+        this.places = ['pizzeria', 'park', 'mall', 'wilderness', 'forest', 'desert', 'swamp', 'permafrost', 'city', 'house', 'trail', 'train', 'mountain',
+                       'restaurant', 'field', 'concert', 'tourist attraction', 'factory', 'alien planet', 'wasteland', 'road', 'reserve', 'school'];
+        this.people = ['Albert Einstein', 'Pope', 'Trump', 'Putin', 'Shrek', 'Dr. Bashir', 'Major Kira', 'Elon Musk', 'Hitler', 'God', 'Popeye'];
     }
     /**
     * @param {Array} arr
@@ -36,6 +42,10 @@ class randomMessage {
                 return this.getRandomItem(this.adjectivesPastTense);
             case 'inter':
                 return this.getRandomItem(this.interminglers);
+            case 'places':
+                return this.getRandomItem(this.places);
+            case 'people':
+                return this.getRandomItem(this.people);
             default:
                 return 'poop';
         }
@@ -43,13 +53,17 @@ class randomMessage {
     returnRandomMessage() {
         const phrases = [`I am ${this.retrieveRandomItemFromList('adjectives')} ${this.retrieveRandomItemFromList('inter')} this ` + 
         `${this.retrieveRandomItemFromList('nouns')} is very ${this.retrieveRandomItemFromList('adjectives')}.`,
-        `Have you ever ${this.retrieveRandomItemFromList('adjPast')} in someone's ${this.retrieveRandomItemFromList('nouns')}?`];
+        `Have you ever ${this.retrieveRandomItemFromList('adjPast')} in someone's ${this.retrieveRandomItemFromList('nouns')}?`,
+        `You are ${this.retrieveRandomItemFromList('adjectives')} ${this.retrieveRandomItemFromList('inter')} ` + 
+        `${this.retrieveRandomItemFromList('adjectives')}.`, `We should hang out in a ${this.retrieveRandomItemFromList('places')}` + 
+        `, it will be ${this.retrieveRandomItemFromList('adjectives')}!`, `What do you think is ${this.retrieveRandomItemFromList('people')}'s ` +
+        `favorite ${this.retrieveRandomItemFromList('nouns')}?`];
 
         return this.getRandomItem(phrases);
     }
 };
 let obj = new randomMessage();
-for(let i = 0; i < 15; i++){
-    console.log('\n' + obj.returnRandomMessage());
-}
+
+console.log('\n' + obj.returnRandomMessage());
+
 
